a41950995894e6f9ff9121603f9d00290b459051
Thomas Villeboeuf <villeboeuf@MacBook-Pro-de-Thomas.local>
2019-10-27 22:24:28 +0100

    add tag event listenner

--- nfcApp/src/app/app.module.ts
+++ nfcApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { NFC, Ndef } from '@ionic-native/nfc/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +18,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    NFC,
+    Ndef
   ],
   bootstrap: [AppComponent]
 })
--- nfcApp/src/app/home/home.page.ts
+++ nfcApp/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+import { NFC, Ndef, NdefEvent } from '@ionic-native/nfc/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,89 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  data: NdefEvent;
 
+  constructor(
+    private platform: Platform,
+    private nfc: NFC,
+    private ndef: Ndef
+    ) {}
+
+  ngOnInit() 
+  {
+    this.platform.ready().then(() => {
+    console.log('ngoninit home');   
+
+    this.nfc.addNdefListener((res) => {
+      console.log('successfully attached ndef listener : '  + JSON.stringify(res));
+    }, (err) => {
+      console.log('error attaching ndef listener : ' +  + JSON.stringify(err));
+    }).subscribe((event) => {
+      console.log('received ndef message. the tag contains: ', event.tag);
+      //console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+      this.data = event;
+    });
+
+    this.nfc.addTagDiscoveredListener((res) => {
+      console.log('successfully attached tag listener : '  + JSON.stringify(res));
+    }, (err) => {
+      console.log('error attaching tag listener : ' +  + JSON.stringify(err));
+    }).subscribe((event) => {
+      console.log('received tag message. the tag is: ', event.tag);
+      //console.log('decoded tag id', this.nfc.bytesToHexString(event.tag.id));
+      this.data = event;
+    });
+
+    });
+  }
+
+  scan(event)
+  {
+    console.log('scan' + event);   
+
+    this.nfc.beginNDEFSession(
+    (res) =>
+    {
+      console.log('successfully start session' + JSON.stringify(res));
+    },
+    (err) =>
+    {
+      console.log('error start session' + JSON.stringify(err));
+    }).subscribe((event) =>
+    {
+      console.log('session subscibe: ', event);
+
+    });
+  }
+
+  write(event)
+  {
+    //console.log('write : ' + event + ' data : ' + this.data.tag);   
+
+    let payload = new Array<number>();
+        payload.push(2);
+        payload.push(10);
+        // payload = payload.concat(this.data.tag.ndefMessage[0].id);
+        // payload = payload.concat(this.data.tag.ndefMessage[0].tnf);
+        // payload = payload.concat(this.data.tag.ndefMessage[0].type);
+        // payload = payload.concat(this.data.tag.ndefMessage[0].payload);
+
+        console.log('connect : ' + payload);
+
+        let message = [this.ndef.record(4, 'toto', '', payload), this.ndef.record(4, 'titi', '', payload)];
+        console.log('payload : ' + JSON.stringify(message));
+
+        this.nfc.write(message)
+        .then(
+        (event) =>
+        {
+          console.log('write promise ok : ' + event);
+    
+        },
+        (error) =>
+        {
+          console.log('write promise fail : ' + error);
+        });
+    
+  }
 }
